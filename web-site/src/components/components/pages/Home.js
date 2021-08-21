82ca8de27a7d98eb3a6212b1fca65ee45764b018
Audrey Le Moine <audreylm33@gmail.com>
2021-08-21 14:26:24 +0200

    footer in home

--- web-site/src/components/components/pages/Home.js
+++ web-site/src/components/components/pages/Home.js
@@ -4,16 +4,18 @@ import Cards from '../Cards';
 import Profile from '../Profile';
 import Banner from '../Banner';
 import Topbar from '../Topbar';
+import Footer from '../Footer';
 
 
 function Home () {
   return (
     <>
-   <Topbar />
-   <Banner/>
-   <Cards />
-   <Banner2 />
-   <Profile />
+  <Topbar />
+  <Banner/>
+  <Cards />
+  <Banner2 />
+  <Profile />
+  <Footer />
    </>
   );
 }
