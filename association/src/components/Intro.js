3f43a63c9f9a4d0b7f4adfb3b59f9ca2a9926d53
Audrey Le Moine <audreylm33@gmail.com>
2021-10-20 17:38:57 +0200

    ajustements

--- association/src/components/Intro.js
+++ association/src/components/Intro.js
@@ -22,7 +22,7 @@ function Intro() {
 
           <p>
 
-          Matriarchy est un collectif qui souhaite créer un système alternatif au patriarcat au travers de retraites, ateliers et contenus pédagogiques. Nous fonctionnons en tant qu’association de type loi 1901 et souhaitons construire un un espace de connexions autour de nos 3 valeurs clés: </p>
+          Matriarchy est un collectif souhaitant créer un système alternatif au patriarcat au travers de retraites, ateliers et contenus pédagogiques. Nous fonctionnons en tant qu’association de type loi 1901 et souhaitons construire un espace de connexions autour de nos 3 valeurs clés: </p>
           <li>féminisme intersectionnel</li>
            <li>sexpositive</li>
            <li>queer</li>
@@ -40,12 +40,12 @@ function Intro() {
         <div className='profile-content'>
           <h1> Un aperçu de ce que Matriarchy propose</h1><br /><br />
           <hr />
-          <blockquote>Le terme subversif Matriarchy est une invitation à réfléchir à un autre modèle de société et à nous questionner sur les constructions (hétéro)normatives. Et ainsi construire un modèle plus inclusif.</blockquote>
+          <blockquote>Le terme subversif Matriarchy est une invitation à réfléchir à un autre modèle de société et à nous questionner sur les constructions (cishétéro)normatives. Et ainsi construire un modèle plus inclusif.</blockquote>
           <hr /><br />
 
           <p>L’organisation se veut non-essentialiste et nous souhaitons éviter toute reproduction de systèmes de domination. </p><br /><br />
 
-          <h1>🌈  Qu’est-ce qu’on y fait ?</h1><br />
+          <h1>🌈   Qu’est-ce qu’on y fait ?</h1><br />
 
           <li> Nous créons des <strong>ateliers</strong> divers en lien avec nos valeurs (cercle de parole, ateliers découverte shibari, ateliers créatifs, burlesque, ateliers sextoys…). Que ce soit dans une optique de découverte, de déconstruction, d’empowerment, nous avons envie de créer du lien et des prises de consciences. </li><br />
           <li><strong>Nos retraites</strong> nous organisons des retraites en non-mixité queer dans lesquelles vous pouvez vous-mêmes proposer des ateliers !</li><br />
@@ -55,13 +55,14 @@ function Intro() {
             <Button className='btn--outline'> Nos évènements</Button><br /><br />
           </a>
 
-          <h1>🧐  Mais ça veut dire quoi tous ces termes militants ?</h1><br />
+          <h1>🧐   Mais ça veut dire quoi tous ces termes militants ?</h1><br />
           <p>Adelphité, intersectionnalité….vous êtes perdu.e.s avec tous ces termes ? Retrouvez notre glossaire dans la page Ressources !
 
             Nous créons aussi du contenu pédagogique pour vous aider à cheminer sur cette voie et acquérir les concepts féministes.</p>
-          <Link to='/Contenus' className='btn-mobile'>
-          <Button className={'btn--outline'}> Nos contenus </Button>
-          </Link>
+
+            <a href='https://drive.google.com/file/d/1tOlFP5raTK6bfzJtR13M1vEI7rqxDVNI/view?usp=sharing'>
+          <Button className={'btn--outline'}> Notre glossaire </Button>
+          </ a>
         </div>
       </div>
     </div>
