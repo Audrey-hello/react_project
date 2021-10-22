ead9e3734d4a28d2442eeea54fd25f19ba695721
Audrey Le Moine <audreylm33@gmail.com>
2021-10-22 10:31:32 +0200

    oya

--- association/src/components/Comite.js
+++ association/src/components/Comite.js
@@ -7,6 +7,7 @@ import Nawa from './1.png';
 import Anahata from './hibou.png';
 import Leonor from './pieuvre.png';
 import Mey from './licorne.png';
+import Oya from './panther.png';
 import Victoire from './chauvesouris.png';
 import Footer from './Footer';
 
@@ -25,13 +26,31 @@ import Footer from './Footer';
             <h5>⎨iel, accords neutres, fém., masc. alternés, non-binaire, queer, bi, blanche⎬ </h5>
           <p>
             <p></p>
-
             <p>Je suis lead communication ainsi que des événements shibari et participe à l’organisation de divers événements au sein de Matriarchy. Je fais parti·e du bureau des membres.</p>
             <p>Féministe, queer. Je participe à la création d’espaces, l’organisation et/ou l’encadrement d’événements shibari et/ou sexpositifs depuis plus de 6 ans (gestion de l’Ecole des cordes, création de l’association French Bonds et de Chatonnade par le passé puis, aujourd'hui, membre actif du Collectif Matriarchy).</p>
           </p><br />
         </div>
         <div>
           <img className='image-profile-comite' src={Nawa} alt='profile' />
+        </div>
+      </div>
+      <div className='profile-grid' id="profile">
+        <div className='comite-content'>
+          <h2>Oya</h2>
+          <h5>⎨pronom il/elle/iel, Genderfluid, bi/pan, afrofeministe, un mix d'Afrique et de Caraïbes⎬</h5>
+
+          <p>
+            <p>Je suis trésorerière de l'association Matriarchy.</p>
+            <p>Le rêve? Une éducation à la sexualité decolonisée.
+              Cela fait quelques années maintenant que je questionne et recherche notre rapport aux corps et comment la sexualité tel que l'on aborde dans nos sociétés occidentales ne nous montre qu'un pan de l'histoire.
+            </p>
+
+            <p>Initialement entrepreneure sociale dans le milieu carcéral et TDS domina, je travaille à offrir des espaces de réflexions et de discussions sur la sexualité et ses rapports sociaux, en particulier dans ses rapports de dominations quel qu'ils soient.
+            </p>
+          </p><br />
+        </div>
+        <div style={{ marginTop: 40 }}>
+          <img className='image-profile-comite' src={Oya} alt='profile' />
         </div>
       </div>
         <div className='profile-grid' id="profile">
@@ -47,7 +66,6 @@ import Footer from './Footer';
               </p>
               <p>Je travaille à produire des réflexions politiques et à structurer les pratiques autour du mouvement sex positive avec l’intention de contribuer à l' émergence d’une culture forte en matière de consentement, d’inclusion, et de santé communautaire dans les collectifs dont je fais parti.e et au niveau communautaire / inter associatif.</p>
             </p><br />
-
           </div>
           <div style={{ marginTop: 40 }}>
             <img className='image-profile-comite' src={Mey} alt='profile' />
@@ -57,7 +75,7 @@ import Footer from './Footer';
         <div className='comite-content'>
           <h2>Anahata </h2>
             <h5>⎨aelle, accords neutres/ fém.,non-binaire, queer, bi ascendant lesbienne, blanche⎬</h5>
-        <p>
+            <p>
             <p>Présidente de l’association et créatrice de l’association, je gère l’organisation interne et des évènements divers. J’écris aussi du contenu sur instagram.</p>
 
             <p>Je fréquente le milieu s+ depuis plus de 2 ans, notamment en ayant créé des retraites dans un collectif. C’était le début de Matriarchy. Ce nom subversif a été créé pour susciter des prises de conscience sur le patriarcat. J’ai voulu apporter ma vision féministe militante dans ce milieu puis j’y ai rencontré de merveilleuses personnes avec qui partager cette vision.  Et c’est ainsi qu’on s’est rassemblé·e·s autour de Matriarchy. Sinon je suis aussi une adepte du shibari et j’adore créer des expériences de déconstruction du patriarcat (ma passion).
